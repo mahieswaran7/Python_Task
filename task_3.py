791c92440f046f0634d018f65a440fa42e57789f
mahieswaran7 <eswaranarumugam2002@gmail.com>
2024-07-23 18:30:39 +0530

    adding python task

--- task_3.py
+++ task_3.py
@@ -1,24 +1,41 @@
-import arithmatic
+def Addition(value1, value2):
+    Ans = value1+value2
+    return Ans
+
+def Subtraction(value1, value2):
+    Ans = value1-value2
+    return Ans
+
+def Multiplication(value1, value2):
+    Ans = value1*value2
+    return Ans
+
+def Division(value1, value2):
+    Ans = value1/value2
+    return Ans
 
 print('Choose operation')
-print('1.Addition\n2.Subtration\n3.Multiplication')
-select = int(input('Select 1/2/3 : ' ))
+print('1.Addition\n2.Subtration\n3.Multiplication\n4.Division')
+select = int(input('Select 1/2/3/4 '))
 
 def main():
-    num01 = int(input('Enter 1st num :'))
-    num02 = int(input('Enter 2st num :'))
+    num01 = int(input('Enter 1st num'))
+    num02 = int(input('Enter 2st num'))
 
     if(select == 1):
-        Ans = arithmatic.Addition(num01,num02)
+        Ans =  Addition(num01,num02)
         print(f'Addition {num01} and {num02}:',Ans)
     elif(select == 2):
-        Ans = arithmatic.Subtraction(num01,num02)
+        Ans =  Subtraction(num01,num02)
         print(f'Subtration {num01} and {num02}:',Ans)
     elif(select == 3):
-        Ans = arithmatic.Multiplication(num01,num02)
+        Ans =  Multiplication(num01,num02)
         print(f'Mutiplication {num01} and {num02}:',Ans)
+    elif(select == 4):
+        Ans =  Division(num01,num02)
+        print(f'Division{num01} and {num02}:',Ans)
     else:
         print('choose 1/2/3')
 
-if __name__=="__main__":
-         main()
+if __name__ =="__main__":
+    main()
